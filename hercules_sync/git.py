f150f3c00a6475dd4a5ac62d7d4ad488b63f9a13
alejgh <alejandrgh11@gmail.com>
2020-02-14 12:27:18 +0100

    Remove the use of tempfiles.
    For now, the use of tempfiles is not necessary. We are saving the 
    content of each file as a string, and returning the GitFile objects in 
    an iterator to save memory.

--- hercules_sync/git.py
+++ hercules_sync/git.py
@@ -1,8 +1,9 @@
+from contextlib import contextmanager
 from urllib.parse import urlencode
 
 import base64
+import io
 import json
-import tempfile
 import urllib3
 
 from unidiff import PatchSet
@@ -22,50 +23,60 @@ class GitEventHandler():
         self.diff_parser = GitDiffParser(self.repo_name,
                                          self.before_commit,
                                          self.after_commit)
+        self.data_loader = GitDataLoader(self.repo_name,
+                                         self.before_commit,
+                                         self.after_commit)
 
+    @property
     def added_files(self):
         added_files = self.diff_parser.patch.added_files
-        return self._git_loader_iterator(added_files, self.after_commit)
+        return self._git_loader_iterator(added_files)
 
+    @property
     def modified_files(self):
         modified_files = self.diff_parser.patch.modified_files
-        return self._git_loader_iterator(modified_files, self.after_commit)
+        return self._git_loader_iterator(modified_files)
 
+    @property
     def removed_files(self):
         removed_files = self.diff_parser.patch.removed_files
-        return self._git_loader_iterator(removed_files, self.before_commit)
+        return self._git_loader_iterator(removed_files)
 
-    def _git_loader_iterator(self, patched_files, ref):
-        with GitDataLoader(self.repo_name, patched_files, ref) as loaded_files:
-            print('Loaded files: ', loaded_files)
-            for i, file in enumerate(loaded_files):
-                yield GitFile(patched_files[i], file.read())
+    def _git_loader_iterator(self, patched_files):
+        for gitfile in self.data_loader.load_files(patched_files):
+            yield gitfile
 
 class GitFile():
     """ Encapsulates the content of a file and the git diff information.
     """
 
-    def __init__(self, patched_file, file_content):
-        self.patched_file = patched_file
-        self.content = file_content
+    def __init__(self, patched_file, source_content, target_content):
+        self._patched_file = patched_file
+        self.source_content = source_content
+        self.target_content = target_content
+
+    @property
+    def file_path(self):
+        return self._patched_file.path
 
     def added_lines(self):
         return [(line.value, line.target_line_no)
-                for hunk in self.patched_file
+                for hunk in self._patched_file
                 for line in hunk if line.is_added]
 
     def removed_lines(self):
         return [(line.value, line.source_line_no)
-                for hunk in self.patched_file
+                for hunk in self._patched_file
                 for line in hunk if line.is_removed]
 
     def _get_lines(self, line_filter):
         return [(line.value, line.target_line_no)
-                for hunk in self.patched_file
+                for hunk in self._patched_file
                 for line in hunk if line_filter(line)]
 
 
 class GitDiffParser():
+
     def __init__(self, repo_name, before_commit, after_commit):
         self.compare_url = self._build_compare_url(repo_name, before_commit,
                                                    after_commit)
@@ -82,10 +93,8 @@ class GitDiffParser():
                 'User-Agent': USER_AGENT
             }
         )
-        # parse response status
+        # TODO: parse response status
         diff = req.data.decode("utf-8")
-        print('Diff: ', diff)
-
         self.patch = PatchSet(diff)
 
     def _build_compare_url(self, repo_name, before_commit, after_commit):
@@ -97,27 +106,25 @@ class GitDiffParser():
         pass
 
 class GitDataLoader():
-    def __init__(self, repo_name, files_to_load, ref=None):
+    """
+    """
+
+    def __init__(self, repo_name, before_ref, after_ref):
         self.repo_name = repo_name
-        self.files_to_load = files_to_load
-        self.ref = ref
-        self.file_pointers = []
-        self.dir_p = tempfile.TemporaryDirectory()
+        self.before_ref = before_ref
+        self.after_ref = after_ref
 
-    def __enter__(self):
+    def load_files(self, files_to_load):
         http = urllib3.PoolManager()
-        return [self._load_file(file_name, http) for file_name
-                in self.files_to_load]
+        return (GitFile(patched_file,
+                        self._load_file(patched_file.path, http, self.before_ref),
+                        self._load_file(patched_file.path, http, self.after_ref))
+                for patched_file in files_to_load)
 
-    def __exit__(self, typ, data, value):
-        for pointer in self.file_pointers:
-            pointer.close()
-        self.dir_p.cleanup()
-
-    def _load_file(self, file_name, http):
-        args = urlencode({'ref': self.ref})
+    def _load_file(self, file_path, http, ref):
+        args = urlencode({'ref': ref})
         download_url = '{0}/repos/{1}/contents/{2}?{3}'.format(
-            GITHUB_API_URL, self.repo_name, file_name.path, args)
+            GITHUB_API_URL, self.repo_name, file_path, args)
         req = http.request(
             'GET',
             download_url,
@@ -125,9 +132,7 @@ class GitDataLoader():
                 'User-Agent': USER_AGENT
             }
         )
-        fp = tempfile.TemporaryFile(dir=self.dir_p.name)
+
         content = json.loads(req.data.decode('utf-8'))['content']
-        decoded_content = base64.b64decode(content)
-        fp.write(decoded_content)
-        self.file_pointers.append(fp)
-        return fp
+        decoded_content = base64.b64decode(content).decode('utf-8')
+        return decoded_content
