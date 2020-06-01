58ebcf705af203dcfb202e4f11fc5de81abfee4a
Alejandro González Hevia <alejandrgh11@gmail.com>
2020-06-01 20:19:19 +0200

    Add BatchOperations class and method to optimize basic operations

--- hercules_sync/synchronization/operations.py
+++ hercules_sync/synchronization/operations.py
@@ -1,22 +1,9 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
 
 from ..triplestore import ModificationResult, TripleStoreManager, TripleElement, TripleInfo
 
 class SyncOperation(ABC):
-    """ Base class of all synchronization operations.
-
-    Parameters
-    ----------
-    sub : TripleElement
-        Subject of the triple to be synchronized.
-    pred : TripleElement
-        Predicate of the triple to be synchronized.
-    obj : TripleElement
-        Object of the triple to be synchronized.
-    """
-    def __init__(self, sub: TripleElement, pred: TripleElement, obj: TripleElement):
-        self._triple_info = TripleInfo(sub, pred, obj)
-
     @abstractmethod
     def execute(self, triple_store: TripleStoreManager) -> ModificationResult:
         """ Executes the operation in the given triple store.
@@ -33,11 +20,26 @@ class SyncOperation(ABC):
 
         """
 
+class BasicSyncOperation(SyncOperation):
+    """ Base class of all the basic synchronization operations.
+
+    Parameters
+    ----------
+    sub : TripleElement
+        Subject of the triple to be synchronized.
+    pred : TripleElement
+        Predicate of the triple to be synchronized.
+    obj : TripleElement
+        Object of the triple to be synchronized.
+    """
+    def __init__(self, sub: TripleElement, pred: TripleElement, obj: TripleElement):
+        self._triple_info = TripleInfo(sub, pred, obj)
+
     def __str__(self):
         return f"{self._triple_info.subject} - {self._triple_info.predicate} " \
                + f"- {self._triple_info.object}"
 
-class AdditionOperation(SyncOperation):
+class AdditionOperation(BasicSyncOperation):
     def execute(self, triple_store: TripleStoreManager) -> ModificationResult:
         return triple_store.create_triple(self._triple_info)
 
@@ -50,7 +52,7 @@ class AdditionOperation(SyncOperation):
 
         return self._triple_info == other._triple_info
 
-class RemovalOperation(SyncOperation):
+class RemovalOperation(BasicSyncOperation):
     def execute(self, triple_store: TripleStoreManager) -> ModificationResult:
         return triple_store.remove_triple(self._triple_info)
 
@@ -62,3 +64,32 @@ class RemovalOperation(SyncOperation):
             return False
 
         return self._triple_info == other._triple_info
+
+class BatchOperation(SyncOperation):
+    """
+    """
+
+    def __init__(subject: TripleElement, triples: List[TripleInfo]):
+        self.subject = subject
+        self.triples = triples
+
+    def execute(self, triple_store: TripleStoreManager) -> ModificationResult:
+        return triple_store.batch_update(self.subject, self.triples)
+
+    def __str__(self):
+        res = [f"BatchOperation: {self.subject}"]
+        for triple in triples:
+            res.append('\n')
+            res.append(str(triple))
+        return ''.join(res)
+
+def optimize_ops(ops: List[BasicSyncOperation]):
+    """
+    """
+    subject_to_triples = defaultdict(list)
+    for op in ops:
+        triple_info = op._triple_info
+        res_ops[triple_info.subject].append(triple_info)
+
+    return [BatchOperation(subject, triples)
+            for subject, triples in subject_to_triples.items()]
